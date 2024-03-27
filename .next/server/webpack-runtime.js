e1114904cb811742175fdb83d4db8c8342d56d6d
stefanpython <stefanhtml@yahoo.com>
2024-03-27 16:38:02 +0200

    Implement pagination in invoices page

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("ff14df38f00e9907")
+/******/ 		__webpack_require__.h = () => ("06f57d9add7ea4d6")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b3e56490ae00d748"; }
+/******/ 		__webpack_require__.h = function() { return "cfc946cc09bd595a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
